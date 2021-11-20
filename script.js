77288e40dba851b619e85db13eaa16ddd4de96d7
Annastark <89winteriscoming@gmail.com>
2021-11-20 12:41:48 +0100

    Array di Oggetti

--- script.js
+++ script.js
@@ -4,3 +4,30 @@
 // Ogni membro dovrà avere le informazioni necessarie per stampare la relativa card: Nome, Ruolo e Foto.
 // Prendendo come riferimento il layout di esempio presente nell'html, stampiamo tutte le card del nostro team.
 // BONUS: Utilizziamo poi gli input presenti nella pagina per permettere all'utente di aggiungere nuovi membri del team.
+
+const team = [{
+        'nome': 'Angela Caroll'
+        'ruolo': 'Chief Editor',
+        'foto': 'img.src="angela-caroll-chief-editor.jpg"'
+    },
+    {
+        'nome': 'Walter Gordon'
+        'ruolo': 'Office Manager',
+        'foto': ''
+    },
+    {
+        'nome': 'Angela Lopez'
+        'ruolo': 'Social Media Manager',
+        'foto': ''
+    },
+    {
+        'nome': 'Scott Estrada'
+        'ruolo': 'Developer',
+        'foto': ''
+    },
+    {
+        'nome': 'Barbara Ramos'
+        'ruolo': 'Graphic Designer',
+        'foto': ''
+    },
+]
